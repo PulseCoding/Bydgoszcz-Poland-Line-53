523fb9b3b4d4b64d910059ed2b1fc15d3f192e1f
Héctor Luis Buenrostro Rivero <hbuenrostrorivero@gmail.com>
2018-09-17 15:02:34 -0500

    Update main_v0.0.1.js

--- main_v0.0.1.js
+++ main_v0.0.1.js
@@ -63,9 +63,9 @@ function idle(){
 }
 
 pubnub = new PubNub({
-  publishKey : "pub-c-ac9f95b7-c3eb-4914-9222-16fbcaad4c59",
-  subscribeKey : "sub-c-206bed96-8c16-11e7-9760-3a607be72b06",
-  uuid: "L53"
+  publishKey : "pub-c-82cf38a9-061a-43e2-8a0f-21a6770ab473",
+  subscribeKey : "sub-c-e14aa146-bab0-11e8-b6ef-c2e67adadb66",
+  uuid : "bydgoszcz-L53-monitoring"
 });
 // --------------------------------------------------------- //
 //Función que realiza las instrucciones de lectura de datos  //
